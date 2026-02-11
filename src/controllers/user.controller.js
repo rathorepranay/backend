aa8e0717e671de7b8927581afdd9bef0e46858b9
rathorepranay <rathorepranay61@gmail.com>
2026-02-11 18:48:30 +0530

    features* added(logout)

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,77 +1,90 @@
 import { User } from "../models/user.models.js";
 import bcrypt from "bcrypt";
 const registerUser = async (req, res) => {
-    try {
-        const { username, email, password } = req.body;
+  try {
+    const { username, email, password } = req.body;
 
-        // Basic validation
-        if (!username || !email || !password) {
-            return res.status(400).json({
-                message: "All fields are required"
-            });
-        }
-
-        // Check if user already exists
-        const existing = await User.findOne({ username: username });
-
-        if (existing) {
-            return res.status(400).json({
-                message: "User already exists!"
-            });
-        }
-
-        // Create user
-        const user = await User.create({
-            username,
-            email,
-            password,
-            loggedIn: false
-        });
-
-        res.status(201).json({
-            message: "User successfully created",
-            user: {
-                id: user._id,
-                email: user.email,
-                username: user.username
-            }
-        });
-
-    } catch (error) {
-        console.log(error);
-        
-        res.status(500).json({
-            message: "Internal Server Error"
-        });
+    // Basic validation
+    if (!username || !email || !password) {
+      return res.status(400).json({
+        message: "All fields are required",
+      });
     }
+
+    // Check if user already exists
+    const existing = await User.findOne({ username: username });
+
+    if (existing) {
+      return res.status(400).json({
+        message: "User already exists!",
+      });
+    }
+
+    // Create user
+    const user = await User.create({
+      username,
+      email,
+      password,
+      loggedIn: false,
+    });
+
+    res.status(201).json({
+      message: "User successfully created",
+      user: {
+        id: user._id,
+        email: user.email,
+        username: user.username,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+
+    res.status(500).json({
+      message: "Internal Server Error",
+    });
+  }
 };
-const loginUser  = async (req,res) => { 
-    try {
-        // check if user already exists
-        const {email,password} = req.body;
-        const user = await User.findOne({
-            email : email.toLowerCase()
-        });
-        if(!user) return res.status(400).json({message:"User not found!"});
+const loginUser = async (req, res) => {
+  try {
+    // check if user already exists
+    const { email, password } = req.body;
+    const user = await User.findOne({
+      email: email.toLowerCase(),
+    });
+    if (!user) return res.status(400).json({ message: "User not found!" });
 
-        // check password
-        const isMatch = await user.comparePassword(password);
-        if(!isMatch) return res.status(400).json({message: "Password mismatch"});
+    // check password
+    const isMatch = await user.comparePassword(password);
+    if (!isMatch) return res.status(400).json({ message: "Password mismatch" });
 
-        res.status(200).json({
-            message : "Login successful",
-            user:{
-                id:user._id,
-                username:user.username,
-                email:user.email
-            }
-        })
-    } catch (error) {
-        console.log(error)
-        res.status(500).json({message:"Internal server error"})
-    }
-}
-export { 
-    registerUser,
-    loginUser
- };
+    res.status(200).json({
+      message: "Login successful",
+      user: {
+        id: user._id,
+        username: user.username,
+        email: user.email,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+const logoutUser = async (req, res) => {
+  try {
+    const { email } = req.body;
+    const user = await User.findOne({ email });
+    if (!user)
+      return res.status(400).json({
+        message: "User not found",
+      });
+    res.status(200).json({
+      message: "Logout successful",
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Internal server Error",
+    });
+  }
+};
+export { registerUser, loginUser,logoutUser };
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,7 +1,9 @@
 import {Router} from 'express';
-import { loginUser,registerUser } from '../controllers/user.controller.js';
+import { loginUser,registerUser,logoutUser } from '../controllers/user.controller.js';
 const router = Router();
 
 router.post('/register',registerUser);
 router.route('/login').post(loginUser);
+router.route('/logout').post(logoutUser);
+
 export default router;
